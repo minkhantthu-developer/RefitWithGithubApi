0aa877875eabecd62eff3478cf5a140fa0f693ef
Min Khant Thu <dsalpha3367@gmail.com>
2024-10-13 17:53:00 +0630

    [Setup] Github Setting

---
+++ RefitWithGithub.Api/GithubSetting.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RefitWithGithub.Api
+{
+    public sealed class GithubSetting
+    {
+        public const string ConfigurationSection = "Github";
+
+        [Required, Url]
+        public string BaseAddress { get; init; } = string.Empty;
+
+        [Required]
+        public string AccessToken { get; init ; }=string.Empty;
+
+        [Required]
+        public string UserAgent { get;init ; }=string.Empty;
+    }
+}
